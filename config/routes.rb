07f5325962736ec21effe731f0d7e77fc8c4d456
Tony Zack <ajzack@gmail.com>
2011-02-28 19:10:37 -0500

    added a user homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Project3::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root :to => "pages#home"
+  
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
