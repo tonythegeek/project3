4ee19bd764973919ce1a52c3ddd240fa3cf3ba64
Tony Zack <ajzack@gmail.com>
2011-03-15 21:30:36 -0400

    added a signin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Project3::Application.routes.draw do
+  
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => "pages#home"
   
@@ -7,6 +9,9 @@ Project3::Application.routes.draw do
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
+  
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   #get "pages/home"
 
   #get "pages/contact"
